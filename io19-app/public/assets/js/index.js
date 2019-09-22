6857520cdd82a9b8081d9953ca8b6b1ca97fe28f
Stanley <angstanley68@gmail.com>
2019-09-22 23:57:01 +0700

    Floating navbar

--- io19-app/public/assets/js/index.js
+++ io19-app/public/assets/js/index.js
@@ -1,6 +1,14 @@
 let includeDiv = $("#content-replace");
 let pathName = window.location.pathname;
 
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 60) {
+        $('#navBar').addClass('floatingNav');
+    } else {
+        $('#navBar').removeClass('floatingNav');
+    }
+});
+
 switch (pathName) {
     case "/home":
         includeDiv.load("includes/home.html");
