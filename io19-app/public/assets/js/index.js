2d677c9ee296e6b6dabaed2a41728016dfb80ba7
Stanley <angstanley68@gmail.com>
2019-09-23 20:40:19 +0700

    Add preloader before page loads

--- io19-app/public/assets/js/index.js
+++ io19-app/public/assets/js/index.js
@@ -1,6 +1,13 @@
 let includeDiv = $("#content-replace");
 let pathName = window.location.pathname;
 
+// Loading spinner
+// Wait for window load
+$(window).on('load', function(){
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});
+
 $(window).scroll(function() {
     if ($(window).scrollTop() > 10) {
         $('#navBar').addClass('floatingNav');
