a453fe8142b310c58dc913badc4eea0d5acf8a1c
Stanley <angstanley68@gmail.com>
2019-09-24 23:17:14 +0700

    Add anchor padding and animation

--- io19-app/public/assets/js/index.js
+++ io19-app/public/assets/js/index.js
@@ -56,3 +56,12 @@ jQuery.ajaxSetup({
   },
   success: function() {}
 });
+
+// Smooth scrolling for anchor
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
